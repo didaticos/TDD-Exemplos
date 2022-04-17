5e0bfb889bf7240c68428d0d2cec11470826adea
Diogo Costa Santos <diogo@codigocentral.com.br>
2022-04-17 14:19:57 -0300

    Adicionar arquivos de projeto.

---
+++ Emprestimos.Core.Tests/Processor/EmprestimosTests.cs
@@ -0,0 +1,48 @@
+﻿using Emprestimos.Core.Domain;
+using Emprestimos.Core.Processor;
+
+using System;
+
+using Xunit;
+
+namespace Emprestimos.Core.Tests.Processor
+{
+    public class EmprestimosTests
+    {
+        EmprestimoProcessor _processor;
+        public EmprestimosTests()
+        {
+            _processor = new EmprestimoProcessor();
+        }
+
+        [Fact]
+        public void DeveRetornarDadosEmprestimosComValoresDaRequisicao()
+        {
+            //organizar requisição
+            var req = new EmprestimoReq()
+            {
+                PrimeiroNome = "Diogo",
+                UltimoNome = "Santos",
+                Email = "diogo@diogo.com",
+                Data = DateTime.Now
+            };
+
+            //processar a requisição e retornar o resultado
+            EmprestimoResult result = _processor.LerDados(req);
+
+            //Afirmação
+            Assert.NotNull(result);
+            Assert.Equal(req.PrimeiroNome, result.PrimeiroNome);
+            Assert.Equal(req.UltimoNome, result.UltimoNome);
+            Assert.Equal(req.Email, result.Email);
+            Assert.Equal(req.Data, result.Data);
+        }
+
+        [Fact]
+        public void DeveRetornarUmaExceptionSeReqForNula()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.LerDados(null));
+            Assert.Equal("req", exception.ParamName);
+        }
+    }
+}
---
+++ Emprestimos.Core/Processor/EmprestimoProcessor.cs
@@ -0,0 +1,25 @@
+﻿using Emprestimos.Core.Domain;
+
+using System;
+
+namespace Emprestimos.Core.Processor
+{
+    public class EmprestimoProcessor
+    {
+        public EmprestimoResult LerDados(EmprestimoReq req)
+        {
+            if (req == null)
+            {
+                throw new ArgumentNullException(nameof(req));
+            }
+
+            return new EmprestimoResult
+            {
+                Data = req.Data,
+                Email = req.Email,
+                PrimeiroNome = req.PrimeiroNome,
+                UltimoNome = req.UltimoNome,
+            };
+        }
+    }
+}
