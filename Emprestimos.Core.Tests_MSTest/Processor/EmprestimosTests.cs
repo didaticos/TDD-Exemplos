d0e5e5875629e67493867093c2f0dbc21f8acca9
Diogo <diogo.costa@fornax.com.br>
2022-04-21 11:09:31 -0300

    adicionado nUnit e MSTest

---
+++ Emprestimos.Core.Tests_MSTest/Processor/EmprestimosTests.cs
@@ -0,0 +1,52 @@
+﻿using Emprestimos.Core.Domain;
+using Emprestimos.Core.Processor;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace Emprestimos.Core.Tests_MSTest.Processor
+{
+
+    [TestClass]
+    public class EmprestimosTests
+    {
+        EmprestimoProcessor _processor;
+
+        [TestInitialize]
+        public void Inicializar()
+        {
+            _processor = new EmprestimoProcessor();
+        }
+
+        [TestMethod]
+        public void DeveRetornarDadosEmprestimosComValoresDaRequisicao()
+        {
+            //organizar requisição
+            var req = new EmprestimoReq()
+            {
+                PrimeiroNome = "Diogo",
+                UltimoNome = "Santos",
+                Email = "diogo@diogo.com",
+                Data = DateTime.Now
+            };
+
+            //processar a requisição e retornar o resultado
+            EmprestimoResult result = _processor.LerDados(req);
+
+            //Afirmação
+            Assert.IsNotNull(result);
+            Assert.AreEqual(req.PrimeiroNome, result.PrimeiroNome);
+            Assert.AreEqual(req.UltimoNome, result.UltimoNome);
+            Assert.AreEqual(req.Email, result.Email);
+            Assert.AreEqual(req.Data, result.Data);
+        }
+
+        [TestMethod]
+        public void DeveRetornarUmaExceptionSeReqForNula()
+        {
+            var exception = Assert.ThrowsException<ArgumentNullException>(() => _processor.LerDados(null));
+            Assert.AreEqual("req", exception.ParamName);
+        }
+    }
+}
---
+++ Emprestimos.Core.Tests_nUnit/Processor/EmprestimosTests.cs
@@ -0,0 +1,50 @@
+﻿using Emprestimos.Core.Domain;
+using Emprestimos.Core.Processor;
+
+using NUnit.Framework;
+
+using System;
+
+namespace Emprestimos.Core.Tests_nUnit.Processor
+{
+    public class EmprestimosTests
+    {
+        EmprestimoProcessor _processor;
+
+        [SetUp]
+        public void Setup()
+        {
+            _processor = new EmprestimoProcessor();
+        }
+
+        [Test]
+        public void DeveRetornarDadosEmprestimosComValoresDaRequisicao()
+        {
+            //organizar requisição
+            var req = new EmprestimoReq()
+            {
+                PrimeiroNome = "Diogo",
+                UltimoNome = "Santos",
+                Email = "diogo@diogo.com",
+                Data = DateTime.Now
+            };
+
+            //processar a requisição e retornar o resultado
+            EmprestimoResult result = _processor.LerDados(req);
+
+            //Afirmação
+            Assert.IsNotNull(result);
+            Assert.AreEqual(req.PrimeiroNome, result.PrimeiroNome);
+            Assert.AreEqual(req.UltimoNome, result.UltimoNome);
+            Assert.AreEqual(req.Email, result.Email);
+            Assert.AreEqual(req.Data, result.Data);
+        }
+
+        [Test]
+        public void DeveRetornarUmaExceptionSeReqForNula()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.LerDados(null));
+            Assert.AreEqual("req", exception.ParamName);
+        }
+    }
+}
